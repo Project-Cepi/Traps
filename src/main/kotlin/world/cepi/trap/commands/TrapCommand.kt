38a4c7b113b51640c07d155048976e4f475f6ce9
LeoDog896 <LeoDog896@hotmail.com>
2021-09-26 11:04:34 -0400

    Add trap help

--- src/main/kotlin/world/cepi/trap/commands/TrapCommand.kt
+++ src/main/kotlin/world/cepi/trap/commands/TrapCommand.kt
@@ -2,6 +2,7 @@ package world.cepi.trap.commands
 
 import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.Player
+import world.cepi.kepi.command.subcommand.applyHelp
 import world.cepi.kstom.command.arguments.delegate
 import world.cepi.kstom.command.arguments.generation.generateSyntaxes
 import world.cepi.kstom.command.arguments.literal
@@ -31,4 +32,17 @@ object TrapCommand : Kommand({
             player.sendMessage("Trap set!")
         }
     }
+
+    applyHelp {
+        """
+            Traps are a way to make blocks emit special properties whenever an action happens to it.
+            
+            Usage:
+            /trap handle (trapName) (trapCoordinates) (...trapProperties)
+            
+            TIP:
+            trapCoordinates automatically autocompletes to the block you are looking at.
+        """.trimIndent()
+    }
+
 }, "trap", "traps")
